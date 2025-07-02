32023a5ba8be41ba466f2824021f59343c1b4f85
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2025-07-02 22:14:44 +0400

    feat: log requests

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+  // Middleware to log requests
+  console.log(`${req.method}: ${req.url}`);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
