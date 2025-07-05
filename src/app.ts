4a72e25bd4ea4d8037ae6d6a11f22711967c9552
hans772 <tobitkjoshy@gmail.com>
2025-07-05 16:34:10 +0530

    fix: fixed .env and naming

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { db } from "./db/client.ts";
+import { users } from "./db/schema.ts";
 
 const app = express();
 
@@ -10,6 +12,12 @@ app.use((req, _, next) => {
 
 app.get("/", (_, res) => {
   res.send("Dev Env Working Properly!");
+});
+
+app.get("/test-db/", async (_, res) => {
+  const allusers = await db.select().from(users);
+
+  res.send(allusers);
 });
 
 app.listen(8000, () => {
--- src/db/user.ts
+++
@@ -1,7 +0,0 @@
-import * as schema from "./schema.ts";
-import * as relations from "./relations.ts";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-
-export const client = postgres(Deno.env.get("DATABASE_URL")!);
-export const db = drizzle(client, { schema });
