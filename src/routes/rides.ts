cadcc54c70a1d1b3fde25254a4b0a00e850d84ab
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2025-07-11 09:37:18 +0400

    chore: format

--- src/routes/rides.ts
+++ src/routes/rides.ts
@@ -3,16 +3,16 @@ import { db } from "../db/client.ts";
 
 const router = express.Router();
 
-router.get("/", async(_, res) => {
+router.get("/", async (_, res) => {
   const rides = await db.query.rides.findMany({
     with: {
-        stops: {
-            orderBy: (stops, {asc}) => asc(stops.order),
-        },
-        rideMembers: true,
-        user: true
-    }
-  })
+      stops: {
+        orderBy: (stops, { asc }) => asc(stops.order),
+      },
+      rideMembers: true,
+      user: true,
+    },
+  });
   res.json(rides);
 });
 
