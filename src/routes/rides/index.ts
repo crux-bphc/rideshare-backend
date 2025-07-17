89b56449ccf1f0963a0f208766010e5b91991bf5
unknown <user.soem1@gmail.com>
2025-07-17 17:27:01 +0530

    feat: Add update route

--- src/routes/rides/index.ts
+++ src/routes/rides/index.ts
@@ -1,3 +1,25 @@
 import z from "zod";
+import { StatusCodes } from "http-status-codes";
+import { HttpError } from "../../utils/http_error.ts";
 
 export const rideIDSchema = z.object({ rideId: z.coerce.number().int() });
+export const ISODateString = z.string().refine(
+  (str) => !isNaN(Date.parse(str)),
+  {
+    message: "Invalid date provided! Expected an ISO date string.",
+  },
+);
+
+export const checkTimes = (startTime: string, endTime: string) => {
+  const start = (new Date(startTime)).getTime(),
+    end = (new Date(endTime)).getTime(),
+    now = Date.now();
+
+  // Don't create rides that run before creation time
+  if (start < now || end < now || end < start) {
+    throw new HttpError(
+      StatusCodes.BAD_REQUEST,
+      "Invalid departure start and / or end time!",
+    );
+  }
+};
