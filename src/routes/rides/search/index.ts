06a43492f9938aad9faac0dc40c3f1955c00a5ed
hans772 <tobitkjoshy@gmail.com>
2025-08-31 17:50:40 +0530

    feat: changed ride respones to give bookmarked status.

--- src/routes/rides/search/index.ts
+++ src/routes/rides/search/index.ts
@@ -2,10 +2,12 @@
 import express, { Request, Response } from "express";
 import { db } from "@/db/client.ts";
 import { asyncHandler } from "@/routes/route_handler.ts";
-import { rides } from "@/db/schema/tables.ts";
+import { rides, userBookmarks } from "@/db/schema/tables.ts";
 import { rideSearchSchema } from "@/validators/ride_validators.ts";
-import { and, asc, between, desc, gt, sql } from "drizzle-orm";
-import { HttpError } from "../../../utils/http_error.ts";
+import { and, asc, between, desc, eq, gt, sql } from "drizzle-orm";
+import { HttpError } from "@/utils/http_error.ts";
+import { StatusCodes } from "http-status-codes";
+import { rideResponseObject } from "../../../utils/response_schemas.ts";
 
 const router = express.Router();
 
@@ -15,6 +17,10 @@ const searchRides = async (req: Request, res: Response) => {
       req.query,
     );
 
+  const { email } = res.locals.user;
+  if (!email) {
+    throw new HttpError(StatusCodes.BAD_REQUEST, "Email was not provided.");
+  }
   // Time search
 
   const tolerance = 1000 * 60 * 60; //One Hour
@@ -42,9 +48,17 @@ const searchRides = async (req: Request, res: Response) => {
     number
   >`similarity(${rides.rideEndLocation}, ${end})`;
 
-  const found_rides = await db.select().from(rides).where(
+  const found_rides = await db.select(rideResponseObject).from(rides).where(
     and(gt(similarityStart, 0), gt(similarityEnd, 0), condition),
-  ).orderBy(desc(sql`greatest(${similarityEnd}, ${similarityStart})`), order);
+  )
+    .leftJoin(
+      userBookmarks,
+      and(
+        eq(userBookmarks.rideId, rides.id),
+        eq(userBookmarks.userEmail, email),
+      ),
+    )
+    .orderBy(desc(sql`greatest(${similarityEnd}, ${similarityStart})`), order);
 
   res.json(found_rides);
 };
